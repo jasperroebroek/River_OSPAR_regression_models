93fc4cc3cbf7c7f7f7dfc6a4a903fc09256bc76c
Jasper Roebroek <jroebroek@MacBook-Pro.homenet.telecomitalia.it>
2021-03-04 11:12:24 +0100

    Optimal lag-time calcution before generating multi-parameter models

--- 03_single_parameter_regression_models.py
+++ 03_single_parameter_regression_models.py
@@ -5,9 +5,21 @@ import pandas as pd
 import statsmodels
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-import geomappy as mp
+import matplotlib.pyplot as plt
 from geomappy.utils import progress_bar
-from scipy.stats import pearsonr
+from sklearn.metrics import r2_score
+
+
+def shuffle_data(data):
+    for col in list(data.columns)[:-1]:
+        np.random.shuffle(data.loc[:, col].values)
+    return data
+
+
+def calc_r2(y, y_predict):
+    mask = ~np.logical_or(np.isnan(y), np.isnan(y_predict))
+    return r2_score(y[mask], y_predict[mask])
+
 
 permutations = 1000
 
@@ -29,24 +41,30 @@ df.loc[:, 'sampling'] = df.Gebiedscode.apply(lambda x: 'Reference' if "REFE" in 
 df.loc[:, 'river'] = df.Gebiedscode.apply(lambda x: x[0])
 df = df.loc[df.river.isin(["W", "M"])]
 df.loc[:, 'all'] = df.loc[:, ITEMS].sum(axis=1)
-df.loc[:, 'season'] = df.meting.apply(lambda x: "Autumn" if x in [0,2] else "Spring")
+df.loc[:, 'season'] = df.meting.apply(lambda x: "Autumn" if x in [0, 2] else "Spring")
+df = df.loc[df.date != 0, :]
 
+df.date = pd.to_datetime(df.date, format="%Y%m%d")
+df.loc[:, 'time_after_last'] = np.nan
 
-def shuffle_data(data):
-    for col in list(data.columns)[:-1]:
-        np.random.shuffle(data.loc[:, col].values)
-    return data
+for i, row in df.iterrows():
+    date_differences = (row.date - df.loc[df.Gebiedscode == row.Gebiedscode, 'date']).dt.days
+    previous_dates = date_differences[date_differences > 0]
+    if previous_dates.size == 0:
+        df.time_after_last.loc[i] = np.nan
+    else:
+        df.time_after_last.loc[i] = previous_dates.min()
 
-
-def calc_r2(x, y):
-    mask = ~np.logical_or(np.isnan(x), np.isnan(y))
-    return pearsonr(x[mask], y[mask])[0] ** 2
-
-
-for parameter in ["P_7d_max", "U_7d_max", "h_7d_max_above_current", "dop", "season", "river", "sampling"]:
+for parameter in ["time_after_last", "dop", "season", "river", "sampling",
+                  "P_2d_max", "P_7d_max", "P_14d_max", "P_1m_max", "P_6m_max",
+                  "U_2d_max", "U_7d_max", "U_14d_max", "U_1m_max", "U_6m_max",
+                  "h_2d_max_above_current", "h_7d_max_above_current", "h_14d_max_above_current",
+                  "h_1m_max_above_current", "h_6m_max_above_current"]:
     print(parameter)
     print("_____________________________________________________________________________")
     for item in ITEMS:
+        print(item)
+
         # check if the file already exists
         if os.path.exists(f"data/item_variance/{parameter}_{item}.csv"):
             continue
@@ -57,11 +75,15 @@ for parameter in ["P_7d_max", "U_7d_max", "h_7d_max_above_current", "dop", "seas
             formula = f'{item} ~ {parameter}'
 
         perm_data = df.loc[:, [parameter, item]]
-        model = smf.glm(formula=formula, data=perm_data, family=sm.families.NegativeBinomial()).fit(full_output=True)
-        true_r2 = calc_r2(perm_data.iloc[:, 1], model.predict(perm_data.iloc[:, 0]))
+        try:
+            model = smf.glm(formula=formula, data=perm_data, family=sm.families.NegativeBinomial()).fit(full_output=True)
+        except (ValueError, statsmodels.tools.sm_exceptions.PerfectSeparationError):
+            continue
+
+        true_r2 = calc_r2(perm_data.iloc[:, 1], model.predict(perm_data))
 
         df_r2 = pd.DataFrame(np.full((permutations, 1), np.nan))
-        print(item)
+
         for i in range(permutations):
             progress_bar((i + 1) / permutations)
             while True:
